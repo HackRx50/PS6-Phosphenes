04522814eb8a6104318f239121db65444f8729d7
HAPPY YADAV <happy.hy0017@gmail.com>
2024-09-13 12:33:00 +0530

    created the component files for  customisation and dashboard

---
+++ hackrx-frontend/src/components/Apages/OverviewPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { GradientLight } from '../design/Benefits'
+import { BarChart2, Menu, Video, FileQuestionIcon } from "lucide-react";
+import Sidebar from './components/Sidebar'
+import Overview from "./OverView"
+
+const DASHBOARD_SIDEBAR_ITEMS = [
+    { name: "Overview", icon: BarChart2, color: "#6366f1", href: "/overview" },
+    { name: "Video Analytics", icon: Video, color: "#8B5CF6", href: "/videoanalytics" },
+    { name: "Quiz Analytics", icon: FileQuestionIcon, color: "#EC4899", href: "/quizanalytics" },
+];
+
+const OverviewPage = () => {
+    return (
+        <div className='flex h-screen bg-n-8 text-gray-100 overflow-hidden'>
+            {/* BG */}
+            <div className='fixed inset-0 z-0'>
+                <div className='absolute inset-0 bg-gradient-to-br opacity-80' />
+                <GradientLight />
+                <div className='absolute inset-0 backdrop-blur-sm' />
+            </div>
+            <Sidebar navItems={DASHBOARD_SIDEBAR_ITEMS} />
+            <Overview />
+        </div>
+    )
+}
+
+export default OverviewPage
