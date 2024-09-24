22f1d10ee490ff80d795e1da4ac7a11e4088634c
HAPPY YADAV <happy.hy0017@gmail.com>
2024-09-24 15:28:07 +0530

    fixed the stable diffusion model

--- hackrx-backend/testmain.py
+++ hackrx-backend/testmain.py
@@ -50,7 +50,7 @@ genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Set path for Tesseract OCR
-pytesseract.pytesseract.tesseract_cmd = r'D:\tesseract\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Define common resolution and frame rate
 common_resolution = (1280, 720)
@@ -176,7 +176,7 @@ def generate_subtitles_from_speech(speech_text, audio_duration, output_srt_path,
 def generate_prompts_from_summary(summary):
     promp_string = ""
     try:
-        inp = model.generate_content(f"Generate 5 Unique and realistic prompts containing 'description' in JSON list fromat for generating the images using AI based on the this summary: {summary} ")
+        inp = model.generate_content(f"Generate 1 Unique and realistic prompt containing 'description' in JSON list fromat for generating the images using AI based on the this summary: {summary} ")
         promp_string = inp.text.strip("\n")
         print(promp_string)
     except Exception as e:
@@ -271,7 +271,7 @@ def trim_video(video_path, duration=7):
 
 def generate_image_from_prompt(prompt, images_folder, image_index):
     headers = {
-        'Authorization': f'Bearer sk-{STABILITY_API_KEY}',
+        'Authorization': f'Bearer sk-YOUR_API_KEY',
         
         'accept': 'image/*'
     }
@@ -302,7 +302,7 @@ def generate_image_from_prompt(prompt, images_folder, image_index):
         response.raise_for_status()  # Raise an error for bad responses
 
         # If receiving the image directly
-        if headers['Accept'] == 'image/*':
+        if headers['accept'] == 'image/*':
             image_data = response.content
             image_path = os.path.join(images_folder, f"generated_image_{image_index}.png")
             with open(image_path, "wb") as img_file:
@@ -343,7 +343,7 @@ def generate_and_save_images_and_videos_for_keywords(keywords):
         params = {
             'key': API_KEY,
             'q': keyword,
-            'video_type': 'all',    # For videos
+            'video_type': 'animation',    # For videos
             'per_page': 3             # Number of results per page
         }
 
