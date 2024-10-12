710cda7da876152a1c760d640cf2c0a67629cfdb
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2024-10-12 17:07:44 +0530

    teamcards fixed

--- hackrx-frontend/src/components/TeamCards.jsx
+++ hackrx-frontend/src/components/TeamCards.jsx
@@ -21,7 +21,7 @@ const Services = () => {
         />
 
         <div className="relative">
-          {/* <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <img
                 className="w-full h-full object-cover md:object-right"
@@ -51,7 +51,7 @@ const Services = () => {
             </div>
 
             <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
-          </div> */}
+          </div>
 
           <div className="relative z-1 grid gap-5 md:grid-cols-2 lg:grid-cols-4 ">
            <div className="relative border border-n-1/10 rounded-3xl overflow-hidden  bg-conic-gradient  h-[30rem] lg:h-[25rem] md:h-[40rem] sm:h-[30rem]">
